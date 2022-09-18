f9af98c97ee6ab1641e3bcc5d42ae73666d0bfa4
YuBinLab-QUST <111045765+YuBinLab-QUST@users.noreply.github.com>
2022-09-18 19:18:06 +0800

    Update PsePSSM.m

--- Feature_extraction/PsePSSM.m
+++ Feature_extraction/PsePSSM.m
@@ -1,7 +1,6 @@
 clear all
 clc
 lamdashu=2;
-%¶ÁÈ¡µ°°×ÖÊĞòÁĞµÄpssm
 WEISHU=3839;
 load('xuliechangdu.mat')
 for i=1:WEISHU
@@ -9,17 +8,15 @@ for i=1:WEISHU
     name = strcat(nnn,'.pssm');
     fid{i}=importdata(name);
 end
-%ËùÓĞµ°°×ÖÊĞòÁĞ¹éÒ»»¯
 c=cell(WEISHU,1);
 for t=1:WEISHU
     clear shu d
 shu=fid{t}.data;
-%ÖªµÀÃ¿Ìõµ°°×ÖÊµÄÊıÁ¿£¬ÌáÈ¡³öÀ´µÄ¾ØÕó£¬×¢Òâµ°°×ÖÊµÄË³Ğò
 % shuju=shu(1:i,1:20);
 [M,N]=size(shu);
 shuju=shu(1:lensec(1,t),1:20);
 d=[];
-%¹éÒ»»¯
+%å½’ä¸€åŒ–
 for i=1:lensec(1,t)
    for j=1:20
        d(i,j)=1/(1+exp(-shuju(i,j)));
@@ -27,32 +24,30 @@ for i=1:lensec(1,t)
 end
 c{t}=d(:,:);
 end
-% %Éú³ÉPSSM-AAC,xÊÇÒ»¸ö,
-% for i=1:WEISHU
-% [MM,NN]=size(c{i});
-%  for  j=1:20
-%    x(i,j)=sum(c{i}(:,j))/MM;
-%  end
-% end
-% %PsePSSMºó20*lamda
-% xx=[];
-% sheta=[];
-% shetaxin=[];
-% % lamda=1;
-% for lamda=1:lamdashu;
-% for t=1:WEISHU
-%   [MM,NN]=size(c{t});
-%   clear xx
-%    for  j=1:20
-%       for i=1:MM-lamda
-%        xx(i,j)=(c{t}(i,j)-c{t}(i+lamda,j))^2;
-%       end
-%       sheta(t,j)=sum(xx(1:MM-lamda,j))/(MM-lamda);
-%    end
-% end
-% shetaxin=[shetaxin,sheta];
-% end
-% psepssm=[x,shetaxin];
-% csvwrite('psepppssm2.csv',psepssm)
-% % save psepppssm10.mat psepssm
+% %ç”ŸæˆPSSM-AAC,xæ˜¯ä¸€ä¸ª,
+for i=1:WEISHU
+[MM,NN]=size(c{i});
+for  j=1:20
+  x(i,j)=sum(c{i}(:,j))/MM;
+  end
+end
+xx=[];
+sheta=[];
+shetaxin=[];
+
+for lamda=1:lamdashu;
+for t=1:WEISHU
+  [MM,NN]=size(c{t});
+  clear xx
+   for  j=1:20
+      for i=1:MM-lamda
+       xx(i,j)=(c{t}(i,j)-c{t}(i+lamda,j))^2;
+      end
+      sheta(t,j)=sum(xx(1:MM-lamda,j))/(MM-lamda);
+   end
+end
+shetaxin=[shetaxin,sheta];
+end
+psepssm=[x,shetaxin];
+csvwrite('psepppssm2.csv',psepssm)
       
